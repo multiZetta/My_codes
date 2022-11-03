e2900abd4cbae2c201b447167b3ce1fa904198a0
multiZetta <multi@Zetta.ru>
2022-11-03 14:48:39 +0400

    Код для посчета количества цифр в числе

---
+++ Count_in_Num/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+// Чтобы посчитать сколько цифр в числе.
+
+Console.WriteLine("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+    
+    int result = 0;
+    while (num > 0)    
+    {
+        result += 1;
+        num = num / 10;
+    }
+Console.WriteLine(result);
